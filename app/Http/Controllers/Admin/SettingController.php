d630fd7e7b833940a97ad895313c33574a8bb734
Hung <hungtph03027@gmail.com>
2022-02-26 05:55:37 +0700

    add

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -11,57 +11,76 @@ class SettingController extends Controller
     private $setting;
     public function __construct(Setting $setting)
     {
-        $this->setting=$setting;
+        $this->setting = $setting;
     }
-    public function show(Request $req){
-        $page= isset($req->page)? $req->page: 1; 
-        $quantity=4;
-        $stt = ($page-1)*$quantity+1;
-        $setting=$this->setting->orderby('id','desc')->paginate($quantity);
-        return view('admin.setting.show',[
-            'title'=> 'Setting',
-            'tieude'=> 'Setting List',
+    public function show(Request $req)
+    {
+        $page = isset($req->page) ? $req->page : 1;
+        $quantity = 4;
+        $stt = ($page - 1) * $quantity + 1;
+        $setting = $this->setting->orderby('id', 'desc')->paginate($quantity);
+        return view('admin.setting.show', [
+            'title' => 'Setting',
+            'tieude' => 'Setting List',
             'settings' => $setting,
-            'stt'=> $stt
+            'stt' => $stt
         ]);
     }
-    public function add(){
-        return view('admin.setting.add',[
-            'title'=> 'Setting',
-            'tieude'=> 'Setting List',
+    public function add()
+    {
+        return view('admin.setting.add', [
+            'title' => 'Setting',
+            'tieude' => 'Setting List',
         ]);
     }
-    public function create(Request $req){
-        $data=$req->validate([
+    public function create(Request $req)
+    {
+        $data = $req->validate([
             'key' => 'required|unique:settings,key',
-            'value_key'=> 'required'
+            'value_key' => 'required'
         ]);
-        $setting=$this->setting->create([
-            'key'=>$data['key'],
-           'value_key' => $data['value_key'],
-            'active' => $req->input('active',0),
-            'type'=> $req->type
+        $setting = $this->setting->create([
+            'key' => $data['key'],
+            'value_key' => $data['value_key'],
+            'active' => $req->input('active', 0),
+            'type' => $req->type
         ]);
-        return redirect()->back()->with('success','Đã thêm thành công Setting '.$setting->key);
+        return redirect()->back()->with('success', 'Đã thêm thành công Setting ' . $setting->key);
     }
-    public function edit($id){
-        $setting=$this->setting::find($id);
-        if($setting==null){
-            return redirect()->back()->with('error','Do not found!!!');
+    public function edit($id)
+    {
+        $setting = $this->setting::find($id);
+        if ($setting == null) {
+            return redirect()->back()->with('error', 'Do not found!!!');
         }
-       return view('admin.setting.edit',[
-           'title'=>'Edit Setting',
-           'tieude'=> 'Sửa Setting',
-           'setting'=> $setting,
-       ]);
+        return view('admin.setting.edit', [
+            'title' => 'Edit Setting',
+            'tieude' => 'Sửa Setting',
+            'setting' => $setting,
+        ]);
     }
-    public function update(Request $req){
-        
-    }
-    public function delete($id){
-       $this->setting::find($id)->delete();
-          return redirect()->back()->with('success','Xóa thành công setting');
+    public function update(Request $req)
+    {
+        $data = $req->validate([
+            'key' => 'required',
+            'value_key' => 'required'
+        ]);
+        $setting = $this->setting::find($req->id);
+        if ($setting == null) {
+            return redirect()->back()->with('error', 'Do not found!!!');
         }
-    
-
+        $type= $req->type ? $req->type:"text";
+        $setting->update([
+            'key' => $data['key'],
+            'value_key' => $data['value_key'],
+            'active' => $req->input('active', 0),
+            'type' => $type
+        ]);
+        return redirect()->back()->with('success', 'Sửa thành công setting');
+    }
+    public function delete($id)
+    {
+        $this->setting::find($id)->delete();
+        return redirect()->back()->with('success', 'Xóa thành công setting');
+    }
 }
